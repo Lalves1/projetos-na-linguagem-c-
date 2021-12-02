8f84f87eb0ca1383028a4c6d536a1ca810558ad9
Lalves1 <93354178+Lalves1@users.noreply.github.com>
2021-12-02 20:34:11 -0300

    Add files via upload

---
+++ gid hub.main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+/*implemente um sistema onde o usuario, entre com 5 valores e mostre quantos deles são repetidos e suas posicoes;
+*/
+// o valor repetido e %;
+// sua posicao e 1 e 3;
+
+using namespace std;
+
+int main()
+{
+	class computador{//nome da classe;
+	int computador[5];//variavel;
+	int j,b,l;
+	public://metado para acessofora da classe
+	computer(int conputer[5]);//declaraçao do metado;
+	};
+	
+	//entradas
+	
+	int computador(int computador[5]){//ação do metado //parametro
+	for(j=0;j<5;j++){
+		cout<<"Digite o"<<j+1<<"valor"<<endl;
+		cin>>vet[i];
+    }
+	
+	// Saidas
+	
+    for (j=0;j<5;j++){
+	};
+    for (j=j<5;j++){  
+    if(computer[j] ==computer[b]){
+    	if(computer[j!= j]){
+    		cout<<"o numero repetido "<<conputer[j]<<"na posicao "<<j+1<<" e "<<j+1<<endl;
+    		break;
+		};
+	}
+	}
+	}
+    }
+    {
+    int main()}
+    	int computer[5];
+    	computador computer;
+    	computador.computer.(computer);
+		return 0;
+	{
+		
+        
+
+
+
+	
+
